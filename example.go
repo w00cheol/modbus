0f861ff0acff128481892d07d637f78f5a35a12a
w00cheol <kw8384@gmail.com>
2023-08-02 10:02:02 +0900

    Docs: example
    rename modbus.go to example.go
    add how to set logger

--- example.go
+++ example.go
@@ -3,29 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"main/modbus"
 )
 
 func main() {
+	// Set loggers
+	modbus.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	modbus.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+
 	// Modbus RTU
 	var address string = "x.x.x.x:502"
 	var timeout time.Duration = 3 * time.Second
 	var slaveId byte = 1
-
-	client, err := modbus.NewClient(modbus.TCP, address, timeout, slaveId, modbus.COIL_DISCRETE, modbus.REGISTER_DISCRETE)
+	// Get client
+	client, err := modbus.NewClient(modbus.TCP, address, timeout, slaveId, modbus.COIL_BIT, modbus.REGISTER_UINT16)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	results, err := client.WriteSingleCoil(0, uint16(modbus.ON))
+	// Call funtions
+	results, err := client.WriteSingleRegister(5, 1)
 	if err != nil {
 		log.Println(err)
 	}
 	fmt.Printf("write: %+v\n", results)
 
-	results, err = client.ReadCoils(0, 10)
+	results, err = client.ReadHoldingRegisters(0, 32)
 	if err != nil {
 		log.Println(err)
 	}
